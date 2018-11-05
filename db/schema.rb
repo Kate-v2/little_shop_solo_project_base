5b22b58fe729f10ea4ebae7d200f596ab8cae1d7
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-05 14:51:49 -0700

    add column for user_address to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105212619) do
+ActiveRecord::Schema.define(version: 20181105214838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20181105212619) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_address_id"
+    t.index ["user_address_id"], name: "index_orders_on_user_address_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 20181105212619) do
   add_foreign_key "items", "users"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "user_addresses"
   add_foreign_key "orders", "users"
   add_foreign_key "user_addresses", "users"
 end
